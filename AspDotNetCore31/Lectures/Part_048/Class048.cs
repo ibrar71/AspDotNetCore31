8d1154227851b4ff6455c014a1d3634082c8ef50
ibrar71 <ibrar.se@gmail.com>
2024-04-01 16:11:02 +0200

    Lectures template folders for 124 lectures added

---
+++ AspDotNetCore31/Lectures/Part_048/Class048.cs
@@ -0,0 +1,12 @@
+//Can be used to save current lecture  
+ 
+using System; 
+  
+namespace AspNetCore31 
+{ 
+    internal class Class048 
+    { 
+        //Code goes here for the current lesson 
+    } 
+}	 
+  
